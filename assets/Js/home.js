3eba40302174758fc130703bb3f80226fc326c82
Camal Zeynallı <zeynalli.camal@mail.ru>
2022-03-12 01:24:12 +0400

    First

---
+++ assets/Js/home.js
@@ -0,0 +1,34 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('.featuredOwl').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
